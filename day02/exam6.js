577f3ab98d6e7c028adc02d1557f91b04fa9747b
so0hub <so0mile02@gmail.com>
2025-12-24 18:19:07 +0900

    251224practice2

---
+++ day02/exam6.js
@@ -0,0 +1,22 @@
+
+// 함수 == 기능 == 여러 코드들의 집합
+
+// 1. 출력 기능/함수
+console.log("안녕하세요1");
+alert("안녕하세요1");
+document.write("<h3> 안녕하세요 </h3>")
+document.querySelector("h3").innerHTML = "안녕하세요2";
+
+// 과정최종흐름 : HTML <--- JS(리액트) <--- 자바/파이썬/노드JS <--- 데이터베이스(영구저장된자료)
+
+
+
+// 2. 입력 기능/함수
+
+let 반환값1 = confirm(); // [확인=true] , [취소=false] 버튼 제공 알림
+let 반환값2 = prompt(); // 입력상자 제공 알림 , 사용자가 입력한 값이 변수에 저장된다.
+
+let 반환값3 = document.querySelector("h3").innerHTML 
+
+// 과정최종흐름 : HTML ---> JS(리액트) ---> 자바/파이썬/노드JS ---> 데이터베이스(영구저장된자료)
+
---
+++ day02/practice2.js
@@ -0,0 +1,44 @@
+
+
+//1. 숫자 자료 
+
+let 제품별가격 = 2000
+console.log( 제품별가격 );
+let 총결제금액 = 4000
+console.log( 총결제금액 );
+let 주문수량 = 3
+console.log( 주문수량 );
+
+//2. 문자열 찾기
+console.log("아메리카노", "스무디/쉐이크", "A-03")
+
+//3. 불리언 찾기
+const 품절여부 = true; console.log( 품절여부 );
+const 영수증출력여부 = false; console.log( 영수증출력여부 );
+const 적립여부 = true; console.log( 적립여부 );
+
+
+// 4. 배열찾기
+let priceAry = [ 2000, 3000, 4000 ];
+console.log( priceAry) 
+
+let categoryobjAry = [ "커피(COFFEE)", "음료(NON COFFFEE)", "주스/에이드(JUICE/ADE)"];
+console.log(categoryobjAry )
+
+let productAry = [ "아메리카노", "뻭S 카페라떼", "바닐라라떼"];
+console.log( productAry )
+
+// 5.객체찾기
+let produtObj = { name : "원조커피", price : 2500, stock : 35 };
+console.log( produtObj )
+
+let orderobj = {주문번호: 10, 결제금액: 3500, 주문날짜 : "2005-03-01" };
+console.log( orderobj )
+
+let selectobj = { 제품명 : "바닐라라떼", 가격: 3700, 총금액: 3700 };
+console.log( selectobj )
+
+
+
+
+
