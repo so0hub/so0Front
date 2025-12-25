2e74e497462785573568743ed9328906db4025cc
so0hub <so0mile02@gmail.com>
2025-12-26 00:40:48 +0900

    practice3-2

---
+++ day02/practice4.js
@@ -0,0 +1,27 @@
+// 문제2
+console.log("[운동 기록]");
+
+console.log("이름");
+
+let username = prompt("성함이 어떻게 되세요?")
+console.log(username);
+
+console.log("운동종류");
+
+let exercise = prompt("오늘 어떤 종류의 운동을 하셨나요?")
+console.log(exercise);
+
+console.log("운동시간");
+
+let time = prompt("운동시간은 총 얼마나 걸리셨나요?")
+console.log(time);
+
+console.log("칼로리");
+
+console.log(time*7)
+
+
+
+
+
+
