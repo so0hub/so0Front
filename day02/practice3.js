828c106912d8b67ce15482d19e05138569674d4e
so0hub <so0mile02@gmail.com>
2025-12-26 00:23:30 +0900

    pratice3-1

---
+++ day02/practice3.js
@@ -0,0 +1,19 @@
+// 문제1
+// [손님에게 이름과 원하는 음료, 그리고 수량을 순서대로 물어본 후, 모든 정보를 조합하여 
+//  바리스타가 볼 수 있도록 콘솔에 주문 내역을 출력하는 프로그램을 만드세요.
+
+
+    console.log("[주문 접수]");
+    console.log("이름");
+    let username = prompt("성함이 어떻게 되세요?")
+    console.log(username);
+    console.log("메뉴");
+    let drink = prompt("어떤 음료를 주문하실 건가요?")
+    console.log(drink);
+    console.log("수량");
+    let ordercount = prompt("몇 잔일까요?")
+    console.log(ordercount);
+
+
+
+
