e4ffa6a367df3ce74dd91abe1febc8244a5c08b1
so0hub <so0mile02@gmail.com>
2026-01-15 14:25:14 +0900

    20260115 02:25 이게 진짜 최종입니다.

--- day15/practice9.js
+++ day15/practice9.js
@@ -1,5 +1,6 @@
 
 
+
 // [1] 메모리 설계
     // 1. 저장할 데이터 객체로 구성
     const departments = [ 
@@ -67,6 +68,7 @@ function departmentPrint( ){
     // 1. 어디에
     const tbody = document.querySelector("tbody");
     // 2. 무엇을 어떻게. 입력받은 것들을 html의 '어디에'에 tr 출력.
+    
     let html = ``;
     for( let index = 0 ; index <= departments.length-1 ; index++){
         let dep = departments[index];
@@ -79,14 +81,14 @@ function departmentPrint( ){
     // 3. 출력
     tbody.innerHTML = html;
 
-    // 부서 Select에 넣기??
-    const Select = document.querySelector(".department");
-    let html1 = '';
-    for( let index = 0 ; index <= departments.length-1 ; index++){
-        let dep = departments[index];
-        html1 += `<option value="${dep.dcode}">${dep.departmentName}</option>`
-    }
-    Select.innerHTML = html1;   // console.log(Select)
+    // // 부서 Select에 넣기??
+    // const Select = document.querySelector(".department");
+    // let html1 = '';
+    // for( let index = 0 ; index <= departments.length-1 ; index++){
+    //     let dep = departments[index];
+    //     html1 += `<option value="${dep.dcode}">${dep.departmentName}</option>`
+    // }
+    // Select.innerHTML = html1;   // console.log(Select)
 }
 
 //3-3] 부서 수정함수
@@ -103,18 +105,18 @@ function departmentUpdate( dcode ){
 
 //3-4] 부서 삭제함수
 function departmentDelete( dcode ){
-    for(let index = 0 ; index <= departments.length-1 ; index++){
-        for(let index = 0 ; index <= staff.length-1 ; index++){
-            if(dcode == departments[index].dcode){
-                if( dcode == staff[index].dcode){ 
-                alert("부서에 소속된 사원이 존재하므로 삭제할 수 없습니다."); 
-                return; }
-            
-            departments.splice(index, 1);
+    
+    for(let index = 0 ; index <= staff.length-1 ; index++){
+        if( dcode == staff[index].dcode){ 
+            alert("부서에 소속된 사원이 존재하므로 삭제할 수 없습니다."); 
+            return; }}
+
+    for(let i = 0 ; i <= departments.length-1 ; i++){
+            if(dcode == departments[i].dcode){
+            departments.splice(i, 1);
             departmentPrint();               
             return;}
         }
-    }
 }
 
 // [4] 사원영역 구현
@@ -126,13 +128,27 @@ function staffAdd( ){
     const staffNames = staffNameDom.value;
     const staffRankDom = document.querySelector(".staffLank");
     const staffRanks = staffRankDom.value;
-    const staffDepartmentDom = document.querySelector(".department");
+    const staffDepartmentDom = document.querySelector("#department");
     const staffDepartment = staffDepartmentDom.value;
     const staffImgDom = document.querySelector(".image");
     const staffImgs = staffImgDom.files[0];
     // 유효성 검사
     if(staffDepartment == 'disabled'){alert("부서를 선택하세요."); return;}
     if(staffNames == "" || staffRanks == ""){alert("이름과 직급 입력은 필수입니다."); return;}
+   
+    // // 부서코드를 부서명으로 저장
+    // let memDepartment = ''; // 부서명 가져오는 변수
+    // for(let j = 0 ; j <= staff.length-1 ; j++){
+    //     const member = staff[j];
+        
+    //     for(let i = 0 ; i <= departments.length-1 ; i++){
+    //         if(member.dcode == departments[i].dcode){
+    //             memDepartment == departments[i].department;
+    //             break;
+    //         }
+    //     }
+    // }
+
     // 2. 객체 구성하기. (입력받은 값 / 식별코드)
     scode += 1;
     
@@ -145,12 +161,163 @@ function staffAdd( ){
          "dcode": staffDepartment
     };
 
+    for( let i = 0 ; i <= staff.length -1 ; i++){
+            if(staff[i] == obj ){ alert("중복입니다."); return;}
+        }
+
     // 4. 화면 새로고침/렌더링
-    staff.push(obj);
+    staff.push(obj); console.log( obj);
     staffPrint();
+} 
+
+
+
+// ======= 아래는 장민서 님이 하신 것 =======
+
+//사원 출력 함수
+staffPrint()
+function staffPrint(){
+    const tbody = document.querySelector(".staffTable tbody"); // html id나 클래스에 따라 바꿔야 하는곳
+    let html = "";
+    for(let i = 0; i<staff.length; i++){ // staff 배열를 순회
+        const member = staff[i]; // staff의 객체 하나를 저장
+        
+        let memberDepartmentName = ""; // 부서명을 가져오기위한 변수
+        for(let j = 0; j < departments.length; j++){ // departments 배열을 순회
+            if(member.dcode == departments[j].dcode){ // 일치하는 부서명을 찾으면
+                memberDepartmentName = departments[j].departmentName; //부서명 저장
+                break;
+            };
+        };
+
+        html += `
+            <tr>
+                <td> <img src ="${member.staffImg == "" ? "https://placehold.co/100" : member.staffImg}"></td>
+                <td>${member.staffName}</td>
+                <td>${memberDepartmentName}</td>
+                <td>${member.staffRank}</td>
+                <td>
+                    <button class="update" onclick="staffFix(${member.scode})">수정</button>
+                    <button class="delete" onclick="staffDel(${member.scode})">삭제</button>
+                </td>
+            </tr>
+        `;
+    };
+    tbody.innerHTML = html; // 상단 이름에 맞춰 변경해야 하는곳
+};
+
+function staffDel(scode){ // 사원 삭제 함수
+    for(let i = 0; i < staff.length; i++){ //staff 배열 순회
+        if(scode == staff[i].scode) { // 매개변수로 받은 사원 코드와 배열의 객체 안에 있는 사원 코드와 같으면
+            staff.splice(i, 1); // 그 부분 삭제
+            break //종료
+        }
+    }
+    staffPrint();// 화면 새로고침
 }
 
 
+function staffFix(scode){ // 사원 수정 함수
+    for(let i = 0; i < staff.length; i++){ //staff 배열 순회
+        if(scode == staff[i].scode) { // 매개변수로 받은 사원 코드와 배열의 객체 안에 있는 사원 코드와 같으면
+            const newStaffName = prompt("수정할 사원명 : "); // 새로운 사원명 받기  
+            const newStaffRank = prompt("수정할 직급 : "); // 새로운 직급 받기
+            staff[i].staffName = newStaffName; // 기존 사원명위치에 새로운 사원명으로 저장
+            staff[i].staffRank = newStaffRank; // 기존 직급 위치에 새로운 직급으로 저장
+            break
+        };
+    };
+    staffPrint();
+}
+
+// 휴가 목록 출력 함수
+vacationPrint()
+function vacationPrint(){
+    const right_bottomDom = document.querySelector("#right_bottom_inner") // html에 따라 바꿔야 하는곳
+    let html = "";
+    for(let i = 0; i<vacationArray.length; i++){ // 휴가 배열를 순회
+        const vacation = vacationArray[i]; // 휴가의 객체 하나를 저장
+        
+        let staffName = ""; // 사원명을 가져오기위한 변수
+        for(let j = 0; j < staff.length; j++){ // 사원 배열을 순회
+            if(vacation.scode == staff[j].scode){ // 일치하는 사원코드을 찾으면
+                staffName = staff[j].staffName; //사원명 저장
+                break;
+            };
+        };
+
+        html += `
+                <div id="box">
+                    <div class="line1">
+                        <div>${staffName}</div><button class="vacationDel" onclick="vacationDel(${vacation.vcode})">신청취소</button>
+                    </div>
+                    <div class="line2">
+                        ${vacation.vacationStart}~${vacation.vacationEnd}
+                    </div>
+                    <div class="line3">
+                        ${vacation.vacationReason}
+                    </div>
+                </div>
+        `;
+    };
+    right_bottomDom.innerHTML = html;
+}
+
+//휴가 신청 삭제 함수
+function vacationDel(vcode){
+    for(let i = 0; i < vacationArray.length; i++){
+        if(vcode == vacationArray[i].vcode) {
+            vacationArray.splice(i,1);
+            break
+        }
+    }
+    vacationPrint();
+}
+
+// 휴가 신청 함수
+let vcode = 3;
+function vacationAdd(){
+    const staffDom = document.querySelector(".staffselect");
+    const selectName = staffDom.value;
+    const startDom = document.querySelector(".startdate")
+    const start = startDom.value;
+    const endDom = document.querySelector(".enddate");
+    const end = endDom.value;
+    const reasonDom = document.querySelector(".reason");
+    const reason = reasonDom.value; 
+    if(selectName == "disabled"){
+        alert("사원을 선택하세요!");
+        return;
+    };
+    if(start == "" || end == ""){
+        alert("날짜를 입력해 주세요!");
+        return;
+    };
+    if(reason == ""){
+        alert("휴가 사유를 적어주세요!");
+        return;
+    }
+    let foundScode = 0; // 찾은 사원코드를 저장할 변수
+    for (let i = 0; i < staff.length; i++) {
+        if (selectName == staff[i].staffName) {
+            foundScode = staff[i].scode; // 이름이 일치하는 사원의 번호를 저장
+            break; // 찾았으니 반복문 종료
+        }
+    }
+    vcode +=1
+    const obj = {
+        "vcode" : vcode,
+        "scode" : foundScode,
+        "vacationStart" : start,
+        "vacationEnd" : end,
+        "vacationReason" : reason
+    };
+    vacationArray.push(obj)
+    vacationPrint();
+    startDom.value = "";
+    endDom.value = "";
+    reasonDom.value = "";
+}
 
 // ======= 아래는 장민서님이 하신 것 =======
 const departments = [ 
